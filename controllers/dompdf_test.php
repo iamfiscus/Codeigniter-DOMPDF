575f68e54d2c37b50a99fc7e700d136323cd2fb7
Jd Fiscus <jdfiscus@gmail.com>
2011-06-22 11:44:27 -0400

    Init

---
+++ controllers/dompdf_test.php
@@ -0,0 +1,27 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Dompdf_test extends CI_Controller {
+
+	/**
+	 * Example: DOMPDF 
+	 *
+	 * Documentation: 
+	 * http://code.google.com/p/dompdf/wiki/Usage
+	 *
+	 */
+	public function index() {	
+		// Load all views as normal
+		$this->load->view('welcome_message');
+		// Get output html
+		$html = $this->output->get_output();
+		
+		// Load library
+		$this->load->library('dompdf_gen');
+		
+		// Convert to PDF
+		$this->dompdf->load_html($html);
+		$this->dompdf->render();
+		$this->dompdf->stream("welcome.pdf");
+		
+	}
+}
